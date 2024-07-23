9956d593ea7ac0da2141d5184976095fa25816c3
Peliah <pelepoupa@gmail.com>
2024-07-23 17:05:34 +0100

    fix: export middleware not middlware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
-export const middlware = async (req: any) => {
+export const middleware = async (req: any) => {
     const res = NextResponse.next();
     const supabase = createMiddlewareClient({ req, res });
     await supabase.auth.getSession();
