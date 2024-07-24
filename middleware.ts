e45419c0f5b9be86db7a787bd54199b73b8e0a55
Peliah <pelepoupa@gmail.com>
2024-07-24 03:14:41 +0100

    UPDATE: Finally understood authentication with supabase

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,43 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
+// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+// import { type NextRequest, NextResponse } from "next/server";
 
-export const middleware = async (req: any) => {
-    const res = NextResponse.next();
-    const supabase = createMiddlewareClient({ req, res });
-    await supabase.auth.getSession();
-    return res;
+// export const middleware = async (req: NextRequest) => {
+//     const res = NextResponse.next();
+//     const supabase = createMiddlewareClient({ req, res });
+//     await supabase.auth.getSession();
+//     return res;
+// }
+
+// export const config = {
+//     matcher: [
+//         /*
+//          * Match all request paths except for the ones starting with:
+//          * - _next/static (static files)
+//          * - _next/image (image optimization files)
+//          * - favicon.ico (favicon file)
+//          * Feel free to modify this pattern to include more paths.
+//          */
+//         '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+
+//     ],
+// }
+
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
+
+export async function middleware(request: NextRequest) {
+    return await updateSession(request)
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * Feel free to modify this pattern to include more paths.
+         */
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    ],
 }
